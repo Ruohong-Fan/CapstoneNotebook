771345387dc151b90302b6414b56a8c70d9d9b93
Ruohong-Fan <ruohong.fan@gmail.com>
2019-07-01 11:00:46 +0800

    Initial commit

---
+++ Ruohong's Capstone Project Notebook.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This is Ruohong's Capstone Project Notebook.
+
+# In[ ]:
+
+
+import pandas as pd
+
+
+# In[ ]:
+
+
+import numpy as np
+
+
+# In[1]:
+
+
+print("Hello Capstone Project Course!")
+
